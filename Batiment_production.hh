58a4493d092efdd5f6490b6a0502341430514c23
NicolasDurand83 <ndurand83@gmail.com>
2021-12-21 12:17:57 +0100

    1

--- Batiment_production.hh
+++ Batiment_production.hh
@@ -9,7 +9,16 @@
 class Batiment_production: public Batiment{
     public:
         Batiment_production();
-        
+        Batiment_production(int x,int y,int longueur,int largeur,int prix,int conso_elec,int pollution,std::string image,int ouvrier_max,int nb_ouvrier,int production_max):
+                _ouvrier_max(ouvrier_max),_nb_ouvrier(nb_ouvrier),_production_max(production_max)
+                {_x=x;
+                _y=y;
+                _longueur=longueur;
+                _largeur=largeur;
+                _conso_elect=conso_elec;
+                _pollution=pollution;
+                _image=image;
+                };
         ~Batiment_production();
     private:
         int _ouvrier_max;
