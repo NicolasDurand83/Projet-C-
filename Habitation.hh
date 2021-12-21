9cb44519602385e0f1eb6bf9a568d9353305bd76
NicolasDurand83 <ndurand83@gmail.com>
2021-12-21 11:49:07 +0100

    1

--- Habitation.hh
+++ Habitation.hh
@@ -10,17 +10,19 @@
 class Habitation :public Batiment{
     public:
         Habitation();
-        Habitation(int x,int y,int longueur,int largeur,int prix,int conso_elec,int pollution,std::string image,int nb_habitant,int dechet):
+        Habitation(int nb_habitant,int dechet):Batiment(){}
+        /*Habitation(int x,int y,int longueur,int largeur,int prix,int conso_elec,int pollution,std::string image,int nb_habitant,int dechet):
                 _x(x), _y(y),_longueur(longueur),_largeur(largeur),
                 _prix(prix),_conso_elect(conso_elec),_image(image),_id(unique_id++),_nb_habitant(nb_habitant),_dechet(dechet)
-                {};
+                {};*/
         ~Habitation();
         std::string info(){
         }
 
 
     private:
-        /*int _x;
+        /*
+        int _x;
         int _y;
         int _longueur;
         int _largeur;
