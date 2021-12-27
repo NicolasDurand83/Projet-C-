aedef1d538411ee668b62fe5581f193194deb057
NicolasDurand83 <ndurand83@gmail.com>
2021-12-27 12:05:33 +0100

    1

---
+++ Habitation.cc
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <vector>
+
+#include "Batiment.hh"
+#include "Habitation.hh"
+
+  std::string info(Habitation H){
+            std::string s="";
+            s=s+ "Longueur :" + std::to_string(H.get_longeur()) + "\0";
+            s=s+ "Largeur :" + std::to_string(H.get_largeur()) + "\0";
+            s=s+ "Prix :" + std::to_string(H.get_prix()) + "\0";
+            s=s+ "Consomation électrique :" + std::to_string(H.get_conso_elec()) + "\0";
+            s=s+ "Pollution :" + std::to_string(H.get_pollution()) + "\0";
+            s=s+ "Nombre de résident :" + std::to_string(H.get_hab()) + "\0";
+            s=s+ "Déchets produits :" + std::to_string(H.get_dechet()) + "\0";
+            return s;
+        }
